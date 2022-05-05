fb5a5c13c91f9de8eeaf95a4de0708f0f8a0cfa6
ForestLeem <627031222@qq.com>
2022-05-05 10:50:53 +0800

    1

---
+++ README.md
@@ -0,0 +1,12 @@
+Project:  ODRL-based Resource Definition in the Context of Business Processes
+
+Environment:  
+    Python3 (PyCharm)
+    Python library requirements: sys, json, pyqt5.
+
+Implementation: 
+    ResEditor, R-A projection(GUI need to be implementd), A-S projection(need to be implementd)
+
+Start:
+    run the file “main.exe” in the Directory RaaS or run the file "main.py"  
+
--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 from UI import Res_main
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-a = {"@123":"123"}
-a["@twst"]  =1
-print(a)
+
 if __name__ == "__main__":
     import sys
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Res_main.Ui_MainWindow(MainWindow)
-
     MainWindow.show()
     sys.exit(app.exec_())
 
