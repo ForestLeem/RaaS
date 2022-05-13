6d2df42677c223b4d43def9a5867f04e149d3424
ForestLeem <627031222@qq.com>
2022-05-13 16:39:12 +0800

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from UI import Res_main
 from PyQt5 import QtCore, QtGui, QtWidgets
 from UI import RtoA
+from UI import AtoS
 
 
 if __name__ == "__main__":
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Res_main.Ui_MainWindow()
     R_A_ui = RtoA.Ui_MainWindow()
-    ui.setup_UI(MainWindow, ui, R_A_ui)
+    A_S_ui = AtoS.Ui_MainWindow()
+    ui.setup_UI(MainWindow, ui, R_A_ui, A_S_ui)
     MainWindow.show()
     sys.exit(app.exec_())
